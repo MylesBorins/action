82fd9994ab580f4491b57ba066ed35f933ae1a2b
Myles Borins <mylesborins@google.com>
2019-02-08 13:30:33 -1000

    it my action now

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM node:slim
 
 # A bunch of `LABEL` fields for GitHub to index
-LABEL "com.github.actions.name"="WIP"
-LABEL "com.github.actions.description"="Set WIP pull requests to pending"
+LABEL "com.github.actions.name"="CheckMate"
+LABEL "com.github.actions.description"="Post a link because it shouldn't be this hard"
 LABEL "com.github.actions.icon"="edit"
 LABEL "com.github.actions.color"="orange"
-LABEL "repository"="http://github.com/wip/action"
-LABEL "homepage"="http://github.com/wip/action"
-LABEL "maintainer"="Gregor Martynus (https://github.com/gr2m)"
+LABEL "repository"="http://github.com/mylesborins/action"
+LABEL "homepage"="http://github.com/mylesborins/action"
+LABEL "maintainer"="Myles Borins (https://github.com/MylesBorins)"
 
 # install
 COPY package*.json ./
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { Toolkit } = require('actions-toolkit')
 const { context, github: { request } } = new Toolkit()
 
-const isWip = /\bwip\b/i.test(context.payload.pull_request.title)
-const newStatus = isWip ? 'pending' : 'success'
+const sha = 'context.payload.pull_request.head.sha';
+const link = isWip ? 'pending' : 'success'
 
 // https://developer.github.com/v3/repos/statuses/#create-a-status
 request('POST /repos/:owner/:repo/statuses/:sha', context.repo({
-  sha: context.payload.pull_request.head.sha,
-  state: newStatus,
-  target_url: 'https://github.com/wip/action',
-  description: isWip ? 'work in progress' : 'ready for review',
-  context: 'WIP (action)'
-}))
+  sha,
+  state: 'success',
+  target_url: 'https://github.com/mylesborins/action',
+  description: `https://storage.googleapis.com/nodejs-dev-staging/${sha}/`,
+  context: 'Staging Link'
+}));
