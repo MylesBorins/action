e2d0faf88e5c9ac47eba9fbfe6b3ae2c4b9137af
Myles Borins <mylesborins@google.com>
2019-02-08 13:41:07 -1000

    yay

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const sha = context.payload.pull_request.head.sha;
 request('POST /repos/:owner/:repo/statuses/:sha', context.repo({
   sha,
   state: 'success',
-  target_url: 'https://github.com/mylesborins/action',
-  description: `https://storage.googleapis.com/nodejs-dev-staging/${sha}/`,
+  target_url: `https://storage.googleapis.com/nodejs-dev-staging/${sha}/`,
+  description: `Click details to preview changes`,
   context: 'Staging Link'
 }));
