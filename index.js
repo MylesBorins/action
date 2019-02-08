ca0700078d7e79c0e0c0868e85ade524fd7d1b50
Myles Borins <mylesborins@google.com>
2019-02-08 13:57:15 -1000

    I have 99 issues yo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { context, github: { request } } = new Toolkit()
 
 const sha = context.payload.pull_request.head.sha;
 
-request('POST /repos/:owner/:repo/pulls/:number/comments', context.repo({
+request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
   number: context.payload.pull_request.number,
   body: `Preview at: https://storage.googleapis.com/nodejs-dev-staging/${sha.slice(0,7)}/index.html`
 }));
