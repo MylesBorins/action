8248349941e8f881771033e255d8920416fd22e9
Myles Borins <mylesborins@google.com>
2019-02-08 14:27:37 -1000

    woop

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ console.log(JSON.stringify(context.payload));
 
 const name = context.payload.name;
 const conclusion = context.payload.conclusion
-const sha = context.payload.check_suite.head_sha;
+const sha = context.payload.head_sha;
 
-
-if (name.startsWith('Build') && conclusion === 'success') {
+if (name.startsWith('Build') && conclusion === 'success' && context.payload.pull_requests[0]) {
   request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
-    number: context.payload.pull_request.number,
+    number: context.payload.pull_requests[0].number,
     body: `Preview at: https://storage.googleapis.com/nodejs-dev-staging/${sha.slice(0,7)}/index.html`
   }));
 }
