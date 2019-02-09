cc32042fc84f71f414c4e0d8c1cddbe525cfb547
Myles Borins <mylesborins@google.com>
2019-02-08 14:04:17 -1000

    lol maybe

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const { Toolkit } = require('actions-toolkit')
 const { context, github: { request } } = new Toolkit()
 
+const name = context.payload.name;
+const conclusion = context.payload.conclusion
 const sha = context.payload.pull_request.head.sha;
 
-request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
-  number: context.payload.pull_request.number,
-  body: `Preview at: https://storage.googleapis.com/nodejs-dev-staging/${sha.slice(0,7)}/index.html`
-}));
+console.log(JSON.stringify(context.payload));
+
+if (name.startsWith('Build') && conclusion === 'success') {
+  request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
+    number: context.payload.pull_request.number,
+    body: `Preview at: https://storage.googleapis.com/nodejs-dev-staging/${sha.slice(0,7)}/index.html`
+  }));
+}
