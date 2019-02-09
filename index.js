85399e92ca7b653507f93b446694746529448f79
Myles Borins <mylesborins@google.com>
2019-02-08 14:09:14 -1000

    woops

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { Toolkit } = require('actions-toolkit')
 const { context, github: { request } } = new Toolkit()
 
+console.log(JSON.stringify(context.payload));
+
 const name = context.payload.name;
 const conclusion = context.payload.conclusion
 const sha = context.payload.check_suite.head_sha;
-
-console.log(JSON.stringify(context.payload));
 
 if (name.startsWith('Build') && conclusion === 'success') {
   request('POST /repos/:owner/:repo/issues/:number/comments', context.repo({
